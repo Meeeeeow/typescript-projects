025850f333b64ee728aad0af2ccef9e6de8abb32
Meeeeeow <kazutokun467@gmail.com>
2022-07-28 12:36:41 +0600

    doing skills section

--- portfolio/portfolio-app/src/components/home.tsx
+++ portfolio/portfolio-app/src/components/home.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Skills from './skills'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Skills/>
   )
 }
 
--- portfolio/portfolio-app/src/components/skills.tsx
+++ portfolio/portfolio-app/src/components/skills.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <section className='skill'>
+      <Container>
+        <Row>
+          <Col>
+            <div className="title">
+              <h2>Skills</h2>
+              <div>
+                  <h2>my Skills</h2>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
 
